b7b3e6ba3b5a6f09d1e470d046839156e8fa35de
Saloni0111-cpu <salonizade09@gmail.com>
2026-01-29 12:51:26 +0530

    designed Navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
+import { HeroComponent } from './hero/hero.component';
+import { ExploreComponent } from './explore/explore.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, HeroComponent, ExploreComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
