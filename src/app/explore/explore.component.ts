cc749fd5dc5762214cbe83ba326c603bf5c1f205
Saloni0111-cpu <salonizade09@gmail.com>
2026-01-31 15:40:50 +0530

    designed Explore component

--- src/app/explore/explore.component.ts
+++ src/app/explore/explore.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-explore',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './explore.component.html',
   styleUrl: './explore.component.scss'
 })
 export class ExploreComponent {
+
+
+  destinationsRows = [
+  [
+    { name: 'Lonavala', img: 'assets/lonavala.webp' },
+    { name: 'Alibaug', img: 'assets/lonavala.webp' },
+    { name: 'Coorg', img: 'assets/lonavala.webp' },
+    { name: 'Udaipur', img: 'assets/lonavala.webp' },
+    { name: 'Jaipur', img: 'assets/lonavala.webp' },
+    { name: 'Nashik', img: 'assets/lonavala.webp' },
+    { name: 'Ooty', img: 'assets/lonavala.webp' },
+    { name: 'Goa', img: 'assets/lonavala.webp' },
+  ],
+  [
+    { name: 'Jaipur', img: 'assets/lonavala.webp' },
+    { name: 'Nashik', img: 'assets/lonavala.webp' },
+    { name: 'Ooty', img: 'assets/lonavala.webp' },
+    { name: 'Goa', img: 'assets/lonavala.webp' },
+    { name: 'Jaipur', img: 'assets/lonavala.webp' },
+    { name: 'Nashik', img: 'assets/lonavala.webp' },
+    { name: 'Ooty', img: 'assets/lonavala.webp' },
+    { name: 'Goa', img: 'assets/lonavala.webp' },
+  ]
+];
 
 }
