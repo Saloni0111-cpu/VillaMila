130f3c1eb3e893c2516f2b7c42d1449f6587b55d
Saloni0111-cpu <salonizade09@gmail.com>
2026-01-31 17:27:32 +0530

    Started card design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HeroComponent } from './hero/hero.component';
 import { ExploreComponent } from './explore/explore.component';
+import { AvailableComponent } from './available/available.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, HeroComponent, ExploreComponent],
+  imports: [RouterOutlet, NavbarComponent, HeroComponent, ExploreComponent, AvailableComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
