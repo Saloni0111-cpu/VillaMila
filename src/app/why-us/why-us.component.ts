7d967be583fad026dbcff8a5b85ad6f3c6841399
Saloni0111-cpu <salonizade09@gmail.com>
2026-02-02 14:21:54 +0530

    designed WHY CHOOSE US section

--- src/app/why-us/why-us.component.ts
+++ src/app/why-us/why-us.component.ts
@@ -10,4 +10,28 @@ import { CommonModule } from '@angular/common';
 })
 export class WhyUsComponent {
 
+
+  whyUs = [
+    {
+      image: "assets/lonavala.webp",
+      quantity: "200+",
+      description: "Happy Guest"
+    },
+    {
+      image: "assets/lonavala.webp",
+      quantity: "200+",
+      description: "Villas & Farmhouse"
+    },
+    {
+      image: "assets/lonavala.webp",
+      quantity: "200+",
+      description: "Locations"
+    },
+    {
+      image: "assets/lonavala.webp",
+      quantity: "200+",
+      description: "Happy Clients"
+    }
+
+  ]
 }
